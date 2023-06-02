c644b06b7c5b81f5793392edf3757c9fe4a37d67
Daniela Gameiro <danielacgameiro@hotmail.com>
2023-06-02 11:16:30 +0100

    Solve exercise 2 of WriteStuff project

---
+++ WriteStuff2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+
+namespace WriteStuff2
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Falta o nome do ficheiro.");
+                return;
+            }
+
+            string filename = args[0];
+
+            try
+            {
+                using (StreamWriter writer = new StreamWriter(filename))
+                {
+                    while (true)
+                    {
+                        string input = Console.ReadLine();
+
+                        if (string.IsNullOrEmpty(input))
+                            break;
+
+                        writer.WriteLine(input);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
